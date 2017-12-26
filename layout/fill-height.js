f4d6e6ce571cb03c53db9c4a2dac961d03e5f9d6
liuxin <475212506@qq.com>
2017-12-26 16:21:52 +0800

    add fill-height extend

---
+++ layout/fill-height.js
@@ -0,0 +1,11 @@
+$.fn.extend({
+    fillHeight: function() {
+        let windowHeight = window.innerHeight;
+        let eleHeight    = this[0].offsetHeight;
+        let top          = this[0].offsetTop;
+
+        let offsetBottom  = windowHeight - eleHeight - top;
+
+        this.height(this.height() + offsetBottom);
+    }
+})
